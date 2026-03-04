0efb2817b43a2ae1bf368311ce2db672596c1d50
Jhon Araujo <tu-correo@ejemplo.com>
2026-03-03 21:44:40 -0400

    registro de pwa y limpieza de permisos

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// El CSS siempre debe ir ARRIBA de App para que los estilos existan al renderizar
 import "./index.css"; 
 import App from "./App.jsx";
+// Línea mágica para registrar la aplicación móvil
+import { registerSW } from 'virtual:pwa-register';
+
+// Registro del Service Worker para que sea instalable
+registerSW({ immediate: true });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
